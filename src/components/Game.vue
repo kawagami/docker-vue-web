5ac8d61bdee2c2e69f4eb21c2b0818929de90c6d
kawagami <joelai1988@gmail.com>
2021-12-11 15:17:52 +0800

    建立監聽鍵盤上下左右的雛型

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="game">
-    這是game
     <div class="test-block"></div>
     <button v-on:click="testBlock">按鈕</button>
-    <div>
-      <button v-if="flag">會不會顯示sgrg465123</button>
-    </div>
   </div>
 </template>
 
@@ -18,15 +14,43 @@ export default {
     };
   },
   methods: {
-    sumSomething: function () {
-      return (this.flag = !this.flag);
+    keyDown: function () {
+      document.onkeydown = (e) => {
+        let el =
+          e || event || window.event || arguments.callee.caller.arguments[0];
+        if (el && el.keyCode == 37) {
+          console.log("左");
+        } else if (el && el.keyCode == 38) {
+          console.log("上");
+        } else if (el && el.keyCode == 39) {
+          console.log("右");
+        } else if (el && el.keyCode == 40) {
+          console.log("下");
+        }
+      };
     },
     testBlock: function () {
-      const test = document.querySelector(".test-block");
-      test.style.backgroundColor = "black";
+      const test = document.querySelector(".game");
+      const one = Math.floor(Math.random() * 255);
+      const two = Math.floor(Math.random() * 255);
+      const three = Math.floor(Math.random() * 255);
+      test.style.backgroundColor = `rgb(${one}, ${two}, ${three})`;
+      // this.addEventListener("keypress", (event) => {
+      //   if (event.keyCode == 40) {
+      //     alert("hi.");
+      //   }
+      // });
+      // const aaa =  document.querySelector('.game')
+      // aaa.style.display = 'none';
       // console.log(123);
       // return true;
     },
+  },
+  // created() {
+  //   console.log("這裡是created");
+  // },
+  mounted() {
+    this.keyDown();
   },
 };
 </script>
@@ -39,9 +63,9 @@ export default {
   background-color: rgb(180, 48, 48);
 }
 
-.test-block {
+/* .test-block {
   width: 100px;
   height: 100px;
   background-color: #fff;
-}
+} */
 </style>
